89bb28d38ac38de91d0417b862493b5b44ad7ae2
ApriliandoAdhi <129729445+ApriliandoAdhi@users.noreply.github.com>
2023-06-10 22:19:24 +0700

    Initial Commit

---
+++ CobaPendasi/CobaPendasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobaPendasiApp.swift
+//  CobaPendasi
+//
+//  Created by Apriliando Adhi Rinaldy on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CobaPendasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
